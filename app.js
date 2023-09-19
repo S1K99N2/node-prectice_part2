9123271c0768e483c9bbef2a3e0e62ba08994e9e
suprabhatkumar <suprabhatniraj@gmail.com>
2023-09-19 23:55:47 +0530

    crud with file system

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+// EXAMPLE 1
 const user = [
     {
         name: "niraj singh",
@@ -27,3 +28,56 @@ http.createServer((req, res) => {
     res.write(JSON.stringify(user));
     res.end();
 }).listen(5000);
+
+
+// EXAMPLE 2
+
+const fs = require('fs');
+
+const input = process.argv;
+
+if (input[2] == "add") {
+    fs.writeFileSync(input[3], input[4])
+} else if (input[2] == 'remove') {
+    fs.unlinkSync(input[3])
+} else {
+    console.log('invalid input');
+
+}
+
+// EXAMPLE -3
+
+const fs = require('fs');
+const path = require('path');
+const dirpath = path.join(__dirname, "views");
+
+const filepath = `${dirpath}/apple.txt`;
+
+// TEXT ADD IN FILE
+fs.writeFileSync(filepath, "sjdfsabfd sjfdbhsbhfd saafdbhsbhkfd sdfbasbfd")
+
+// READ
+
+fs.readFile(filepath, 'UTF-8', (err, res) => {
+    console.log(res);
+})
+
+// UPDATE
+
+fs.appendFile(filepath, ' and this is simpla text you write', (err) => {
+    if (!err) {
+        console.log('file is updated');
+    }
+})
+
+// RENAME
+
+fs.rename(filepath, `${dirpath}/fruite.txt`, (err) => {
+    if (!err) {
+        console.log('file if update');
+    }
+})
+
+// DELETE
+
+fs.unlinkSync(`${dirpath}/fruite.txt`)
